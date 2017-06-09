1a3e9f3c5674bf7a6e40a6ebaf7351c08cd033d8
maximilian.hochstein <max@BLACKBOARD01>
2017-06-09 12:21:02 +0200

    Removed dependency to Microsoft.Kinect in test application

--- CaptureKinectStream/test/Program.cs
+++ CaptureKinectStream/test/Program.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using CaptureKinectStream;
-using System.IO;
-using System.Text;
-using KinectDummy;
-using System.Threading;
 
 namespace test
 {
